fc534f67378dd11fc0769a34e00f0bd842846b9a
Santii05-C <santi.c.pgm@gmail.com>
2024-07-11 12:48:01 -0300

    last commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,25 @@ import Menu from "./Menu";
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xlg:px-64">
-      {/* MOBILE */}
-
-      <Link href="/">LAMA</Link>
-      <Menu />
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xlg:px-64 relative">
+      <div className="h-full flex items-center justify-between">
+        {/* MOBILE */}
+        <Link href="/">
+          <div className="text-2xl tracking-wide">LAMA</div>
+        </Link>
+        <Menu />
+      </div>
+      {/* BIGGER SCREENS */}
+      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+        <div className="">
+          {/* LEFT */}
+          <div className="w-1/3">
+            <div className="text-2xl tracking-wide">LAMA</div>
+          </div>
+          {/* RIGHT */}
+          <div className="w-2/3"></div>
+        </div>
+      </div>
     </div>
   );
 };
