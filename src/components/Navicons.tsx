67387127453538e7df78806364ce239474b788d5
Santii05-C <santi.c.pgm@gmail.com>
2024-07-12 10:22:04 -0300

    last commit

--- src/components/Navicons.tsx
+++ src/components/Navicons.tsx
@@ -1,7 +1,33 @@
 "use client";
 
+import Image from "next/image";
+
 const NavIcons = () => {
-  return <div className="">NavIcons</div>;
+  return (
+    <div className="">
+      <Image
+        src="/profile.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+      <Image
+        src="/notification.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+      <Image
+        src="/cart.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+    </div>
+  );
 };
 
 export default NavIcons;
