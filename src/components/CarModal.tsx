23ba7a0ac8a0553fb383f714a61e7b7f4dc55955
Santii05-C <santi.c.pgm@gmail.com>
2024-07-12 18:13:40 -0300

    last commit

--- src/components/CarModal.tsx
+++ src/components/CarModal.tsx
@@ -3,27 +3,89 @@
 import Image from "next/image";
 
 const CarModal = () => {
-  const cartItems = false;
+  const cartItems = true;
 
   return (
-    <div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
       {!cartItems ? (
         <div className="">Card is Empty</div>
       ) : (
-        <div className="">
-          <Image
-            src="https://www.pexels.com/es-es/foto/madera-paisaje-agua-verano-24245175/"
-            alt=""
-            width={72}
-            height={96}
-            className="object-cover rounded-md"
-          />
-          {/* TOP */}
-          {/* TITLE */}
-
-          {/* DESC */}
+        <>
+          {/* LIST */}
+          <div className="flex flex-col gap-8">
+            {/* ITEM */}
+            <div className="flex gap-4 ">
+              <Image
+                src="https://images.pexels.com/photos/13144285/pexels-photo-13144285.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                {/* TOP */}
+                <div className="">
+                  {/* TITLE */}
+                  <div className=" flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                  </div>
+                  {/* DESC */}
+                  <div className="tex-sm text-gray-500">available</div>
+                </div>
+                {/* BOTTOM */}
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-4 ">
+              <Image
+                src="https://images.pexels.com/photos/13144285/pexels-photo-13144285.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                {/* TOP */}
+                <div className="">
+                  {/* TITLE */}
+                  <div className=" flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                  </div>
+                  {/* DESC */}
+                  <div className="tex-sm text-gray-500">available</div>
+                </div>
+                {/* BOTTOM */}
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
+              </div>
+            </div>
+          </div>
           {/* BOTTOM */}
-        </div>
+          <div className="">
+            <div className="flex items-center justify-between font-semibold">
+              <span className="">Subtotal</span>
+              <span className="">$49</span>
+            </div>
+            <p className="text-gray-500 text-sm mt-2 mb-4">
+              Lorem ipsum dolor sit amet, consectetur.
+            </p>
+            <div className="flex justify-between text-sm">
+              <button className=" rounded-md py-3 px-4 ring-1 ring-gray-300">
+                View Cart
+              </button>
+              <button className="rounded-md py-3 px-4 bg-black text-white">
+                Checkout
+              </button>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import NavIcons from "./Navicons";
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xlg:px-64 relative">
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       <div className="h-full flex items-center justify-between md:hidden">
         {/* MOBILE */}
         <Link href="/">
