1fe549a781c0ff1bcc54fd34f4ac45db1e2a3ae2
Santii05-C <santi.c.pgm@gmail.com>
2024-07-12 10:42:59 -0300

    last commit

--- src/components/Navicons.tsx
+++ src/components/Navicons.tsx
@@ -1,10 +1,26 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 const NavIcons = () => {
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const router = useRouter();
+
+  //TEMPORARY
+  const isLoggedIn = false;
+
+  const handleProfile = () => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    }
+  };
   return (
-    <div className="">
+    <div className=" flex items-center gap-4 xl:gap-6">
       <Image
         src="/profile.png"
         alt=""
@@ -12,6 +28,14 @@ const NavIcons = () => {
         height={22}
         className="cursor-pointer"
       />
+      {isProfileOpen && (
+        <div className="">
+          <div className="">
+            <Link href="/">Profile</Link>
+            <div className="mt-2 cursor-pointer">Logout</div>
+          </div>
+        </div>
+      )}
       <Image
         src="/notification.png"
         alt=""
