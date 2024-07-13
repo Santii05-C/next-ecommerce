65ef9d2f4e81ca8d59df2d5b74a7f56688d20b86
Santii05-C <santi.c.pgm@gmail.com>
2024-07-13 20:07:13 -0300

    last commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,8 @@ import NavIcons from "./Navicons";
 const Navbar = () => {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* MOBILE */}
       <div className="h-full flex items-center justify-between md:hidden">
-        {/* MOBILE */}
         <Link href="/">
           <div className="text-2xl tracking-wide">LAMA</div>
         </Link>
@@ -17,9 +17,9 @@ const Navbar = () => {
       {/* BIGGER SCREENS */}
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
         {/* LEFT */}
-        <div className="w-1/3 xl:w-1/2 flex items-center gap-3">
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
           <Link href="/" className="flex items-center gap-3">
-            <Image src="/logo.png" alt="" width={24} height={24}></Image>
+            <Image src="/logo.png" alt="" width={24} height={24} />
             <div className="text-2xl tracking-wide">LAMA</div>
           </Link>
           <div className="hidden xl:flex gap-4">
