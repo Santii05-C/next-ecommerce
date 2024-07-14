61b7100eea53b6630dc1937a356c87b831f762fe
Santii05-C <santi.c.pgm@gmail.com>
2024-07-14 12:20:34 -0300

    last commit

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -6,11 +6,11 @@ const ProductList = () => {
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
       <Link
         href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:[22$]"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
       >
         <div className="relative w-ful h-80">
           <Image
-            src="https://images.pexels.com/photos/19640282/pexels-photo-19640282/free-photo-of-balade-d-hiver.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            src="https://images.pexels.com/photos/26690666/pexels-photo-26690666/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
             alt=""
             fill
             sizes="25vw"
@@ -21,7 +21,7 @@ const ProductList = () => {
             alt=""
             fill
             sizes="25vw"
-            className="absolute object-cover"
+            className="absolute object-cover rounded-md"
           />
         </div>
         <div className="flex justify-between">
@@ -29,6 +29,212 @@ const ProductList = () => {
           <span className="font-semibold">$49</span>
         </div>
         <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-ful h-80">
+          <Image
+            src="https://images.pexels.com/photos/26690666/pexels-photo-26690666/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/20852606/pexels-photo-20852606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-ful h-80">
+          <Image
+            src="https://images.pexels.com/photos/26690666/pexels-photo-26690666/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/20852606/pexels-photo-20852606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-ful h-80">
+          <Image
+            src="https://images.pexels.com/photos/26690666/pexels-photo-26690666/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/20852606/pexels-photo-20852606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-ful h-80">
+          <Image
+            src="https://images.pexels.com/photos/26690666/pexels-photo-26690666/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/20852606/pexels-photo-20852606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-ful h-80">
+          <Image
+            src="https://images.pexels.com/photos/26690666/pexels-photo-26690666/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/20852606/pexels-photo-20852606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-ful h-80">
+          <Image
+            src="https://images.pexels.com/photos/26690666/pexels-photo-26690666/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/20852606/pexels-photo-20852606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-ful h-80">
+          <Image
+            src="https://images.pexels.com/photos/26690666/pexels-photo-26690666/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/20852606/pexels-photo-20852606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
       </Link>
     </div>
   );
