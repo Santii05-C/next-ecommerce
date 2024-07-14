8bae163ed6b937336238076238ce3b7b813340dd
Santii05-C <santi.c.pgm@gmail.com>
2024-07-14 12:39:06 -0300

    last commit

---
+++ src/components/CategoryList.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const CategoryList = () => {
+  return (
+    <div className="px-4 overflow-x-scroll">
+      <div className="flex gap-4 md:gap:8">
+        <Link href="/list?cat=test">
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image src="" alt="" fill sizes="20vw" className="object-cover" />
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
