e7b157c0444f48bbadd3d6fc0ef72074a1ea6061
Santii05-C <santi.c.pgm@gmail.com>
2024-07-14 11:30:20 -0300

    last commit

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,14 +3,32 @@ import Link from "next/link";
 
 const ProductList = () => {
   return (
-    <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link href="/test" className="relative w-ful h-80">
-        <Image
-          src="https://images.pexels.com/photos/13144285/pexels-photo-13144285.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-          alt=""
-          fill
-          sizes="25vw"
-        />
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:[22$]"
+      >
+        <div className="relative w-ful h-80">
+          <Image
+            src="https://images.pexels.com/photos/19640282/pexels-photo-19640282/free-photo-of-balade-d-hiver.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/20852606/pexels-photo-20852606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
       </Link>
     </div>
   );
