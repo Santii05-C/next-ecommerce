81c2c28efaecb037114d22d5fee8215f4ddfc30a
Santii05-C <santi.c.pgm@gmail.com>
2024-07-15 13:11:56 -0300

    last commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div className="">Footer</div>;
+  return (
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24">
+      {/* TOP */}
+      <div className="flex justify-between gap-24">
+        {/* LEFT */}
+        <div className="w-1/4 flex flex-col gap-8">
+          <Link href="/">
+            <div className="text-2xl tracking-wide">LAMA</div>
+          </Link>
+          <p>
+            3252 Winding Way, Central Plaze, Willowbrook, CA 90210, United
+            States
+          </p>
+          <span className="font-semibold">hello@lama.dev</span>
+          <span className="font-semibold">+1 234 567 890</span>
+          <div className="flex gap-6">
+            <Image src="/facebook.png" alt="" width={16} height={16} />
+            <Image src="/instagram.png" alt="" width={16} height={16} />
+            <Image src="/youtube.png" alt="" width={16} height={16} />
+            <Image src="/pinterest.png" alt="" width={16} height={16} />
+            <Image src="/x.png" alt="" width={16} height={16} />
+          </div>
+        </div>
+        {/* CENTER */}
+        <div className="w-1/2"></div>
+        {/* RIGHT */}
+        <div className="w-1/4"></div>
+      </div>
+      {/* BUTTOM */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Footer;
