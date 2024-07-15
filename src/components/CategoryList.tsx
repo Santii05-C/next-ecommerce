5589ea61551ff69610cf985bbbc10e5639991ba8
Santii05-C <santi.c.pgm@gmail.com>
2024-07-14 22:01:00 -0300

    last commit

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -3,9 +3,199 @@ import Link from "next/link";
 
 const CategoryList = () => {
   return (
-    <div className="px-4 overflow-x-scroll">
+    <div className="px-4 overflow-x-scroll scroll-hide">
       <div className="flex gap-4 md:gap-8">
-        <Link href="/list?cat=test">
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
           <div className="relative bg-slate-100 w-full h-96">
             <Image
               src="https://images.pexels.com/photos/27060350/pexels-photo-27060350/free-photo-of-verano-jardin-animal-hierba.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
