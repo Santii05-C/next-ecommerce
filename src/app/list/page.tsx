f809af6d5a376454c004a593cebf59d30add9654
Santii05-C <santi.c.pgm@gmail.com>
2024-07-15 23:08:36 -0300

    last commit

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+
 const ListPage = () => {
-  return <div className="">ListPage</div>;
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* CAMPAING */}
+      <div className="">
+        <div className="w-2/3">
+          <h1>Grab up to 50% off on Selected Products</h1>
+          <button>Buy Now</button>
+        </div>
+        <div className="relative w-1/3">
+          <Image src="/woman.png" alt="" fill className="object-cover" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListPage;
