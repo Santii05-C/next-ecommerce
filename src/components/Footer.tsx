8631f71875e302a785496abc34ab563fb50d65f5
Santii05-C <santi.c.pgm@gmail.com>
2024-07-15 22:15:24 -0300

    last commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24">
       {/* TOP */}
-      <div className="flex justify-between gap-24">
+      <div className="flex flex-col md:flex-row justify-between gap-24">
         {/* LEFT */}
         <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
           <Link href="/">
@@ -26,9 +26,63 @@ const Footer = () => {
           </div>
         </div>
         {/* CENTER */}
-        <div className="hidden lg:flex justify-between w-1/2"></div>
+        <div className="hidden lg:flex justify-between w-1/2">
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">COMPANY</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">About Us</Link>
+              <Link href="">Careers</Link>
+              <Link href="">Affiliates</Link>
+              <Link href="">Blog</Link>
+              <Link href="">Contact Us</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">SHOP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">New Arrivals</Link>
+              <Link href="">Accessories</Link>
+              <Link href="">Men</Link>
+              <Link href="">Woman</Link>
+              <Link href="">All Products</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">HELP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">Customer Service</Link>
+              <Link href="">My Account</Link>
+              <Link href="">Find a Store</Link>
+              <Link href="">Legal & Pivacy</Link>
+              <Link href="">Gift Card</Link>
+            </div>
+          </div>
+        </div>
+
         {/* RIGHT */}
-        <div className="w-1/4"></div>
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <h1 className="font-medium text-lg">SUBSCRIBE</h1>
+          <p className="">
+            Be the first to get the latest news about trends, promotions, and
+            much more!
+          </p>
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="Email address"
+              className="p-4 w-3/4"
+            />
+            <button className="w-1/4 bg-lama text-white">JOIN</button>
+          </div>
+          <span className="font-semibold">Secure Payments</span>
+          <div className="flex justify-between">
+            <Image src="/discover.png" alt="" width={40} height={20} />
+            <Image src="/skrill.png" alt="" width={40} height={20} />
+            <Image src="/paypal.png" alt="" width={40} height={20} />
+            <Image src="/mastercard.png" alt="" width={40} height={20} />
+            <Image src="/visa.png" alt="" width={40} height={20} />
+          </div>
+        </div>
       </div>
       {/* BUTTOM */}
       <div className=""></div>
