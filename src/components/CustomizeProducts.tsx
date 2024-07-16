f11beee07fe9490c349ce8b9b5212ee9ca2d0048
Santii05-C <santi.c.pgm@gmail.com>
2024-07-16 16:27:59 -0300

    last commit

---
+++ src/components/CustomizeProducts.tsx
@@ -0,0 +1,5 @@
+const CustomizeProducts = () => {
+  return <div className="">CustomizeProducts</div>;
+};
+
+export default CustomizeProducts;
--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,46 +1,57 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 const images = [
   {
     id: 1,
-    url: "https://www.pexels.com/es-es/foto/ciudad-viaje-viajar-callejon-18460661/",
+    url: "https://images.pexels.com/photos/18460661/pexels-photo-18460661/free-photo-of-ciudad-viaje-viajar-callejon.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
   },
   {
     id: 2,
-    url: "https://www.pexels.com/es-es/foto/madera-verano-animal-mono-27000642/",
+    url: "https://images.pexels.com/photos/19820544/pexels-photo-19820544/free-photo-of-nieve-nevar-madera-paisaje.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
   },
   {
     id: 3,
-    url: "https://www.pexels.com/es-es/foto/resfriado-frio-nieve-nevar-19342868/",
+    url: "https://images.pexels.com/photos/18939401/pexels-photo-18939401/free-photo-of-blanco-y-negro-ciudad-coches-trafico.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
   },
   {
     id: 4,
-    url: "https://www.pexels.com/es-es/foto/mariposa-16741638/",
+    url: "https://images.pexels.com/photos/20877003/pexels-photo-20877003/free-photo-of-ciudad-edificios-pasos-urbano.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
   },
 ];
 
 const ProductImages = () => {
+  const [index, setIndex] = useState(0);
+
   return (
     <div className="">
       <div className="h-[500px] relative">
         <Image
-          src="https://images.pexels.com/photos/52500/horse-herd-fog-nature-52500.jpeg?auto=compress&cs=tinysrgb&w=600"
+          src={images[index].url}
           alt=""
           fill
           sizes="50vw"
           className="object-cover rounded-md"
         />
       </div>
-      <div className="">
-        <div className="w-1/4 h-32 relative gap-4 mt-8">
-          <Image
-            src="https://images.pexels.com/photos/52500/horse-herd-fog-nature-52500.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt=""
-            fill
-            sizes="30vw"
-            className="object-cover rounded-md"
-          />
-        </div>
+      <div className="flex justify-between gap-4 mt-8">
+        {images.map((img, i) => (
+          <div
+            className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
+            key={img.id}
+            onClick={() => setIndex(i)}
+          >
+            <Image
+              src={img.url}
+              alt=""
+              fill
+              sizes="30vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
