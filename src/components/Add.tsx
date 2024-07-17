54d9d8304b5fee24607785a711e1b2bace0ad183
Santii05-C <santi.c.pgm@gmail.com>
2024-07-17 18:37:27 -0300

    last commit

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,12 +1,23 @@
+"use client";
+
+import { useState } from "react";
+
 const Add = () => {
+  const [quantity, setQuantity] = useState(1);
+
   return (
     <div className="flex flex-col gap-4">
       <h4 className="font-medium">Choose a Quantity</h4>
       <div className="">
-        <button className="w-36 text-sm rounded-3xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">
-          Add to Cart
-        </button>
+        <div className=""></div>
+        <div className="">
+          Only <span className="text-orange-500 ">4 items</span> left! {"Don't"}{" "}
+          misst it
+        </div>
       </div>
+      <button className="w-36 text-sm rounded-3xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">
+        Add to Cart
+      </button>
     </div>
   );
 };
