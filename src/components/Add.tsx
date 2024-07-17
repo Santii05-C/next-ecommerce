ed7cca668d9a9dd7da8618ac8e8785dc8ec7a8e4
Santii05-C <santi.c.pgm@gmail.com>
2024-07-17 18:09:41 -0300

    last commit

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,5 +1,14 @@
 const Add = () => {
-  return <div className="">Add</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h4 className="font-medium">Choose a Quantity</h4>
+      <div className="">
+        <button className="w-36 text-sm rounded-3xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Add;
