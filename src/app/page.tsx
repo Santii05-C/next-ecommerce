f7a28ec72534d61a86ea9368d2e6a6d95916af97
Santii05-C <santi.c.pgm@gmail.com>
2024-07-18 20:16:12 -0300

    last commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,19 @@ import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 import { WixClientContext } from "@/context/wixContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 const HomePage = () => {
   const wixClient = useContext(WixClientContext);
-  const getProducts = async () => {
-    const res = await wixClient.products.queryProducts().find();
-  };
 
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await wixClient.products.queryProducts().find();
+
+      console.log(res);
+    };
+    getProducts();
+  }, [wixClient]);
   return (
     <div className="">
       <Slider />
