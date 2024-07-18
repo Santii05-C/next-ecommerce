9eeecbce51815f0918a317b107e1fa4997a9b7d8
Santii05-C <santi.c.pgm@gmail.com>
2024-07-17 22:40:01 -0300

    last commit

---
+++ src/components/santy.tsx
@@ -0,0 +1,12 @@
+const Santy = () => {
+  return (
+    <div className="">
+      <div className="">
+        <h1>hola</h1>
+        <h2>Bienvenido a casa</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Santy;
