247b7788e5f6179c4acbab5415f9f6608b7fb640
Santii05-C <santi.c.pgm@gmail.com>
2024-07-19 17:16:54 -0300

    last commit

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,55 +5,38 @@ import Link from "next/link";
 
 const PRODUCT_PER_PAGE = 20;
 
-const ProductList = async ({
-  categoryId,
-  limit,
-}: {
-  categoryId: string,
-  limit?: number,
-}) => {
-  const wixClient = await wixClientServer();
-
-  const res = await wixClient.products
-    .queryProducts()
-    .eq("collectionIds", categoryId)
-    .limit(limit || PRODUCT_PER_PAGE)
-    .find();
-
+const ProductList = () => {
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      {res.items.map((product: products.Product) => (
-        <Link
-          href="/test"
-          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-          key={product._id}
-        >
-          <div className="relative w-ful h-80">
-            <Image
-              src="https://images.pexels.com/photos/26690666/pexels-photo-26690666/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-            />
-            <Image
-              src="https://images.pexels.com/photos/20852606/pexels-photo-20852606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md"
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="font-medium">Product Name</span>
-            <span className="font-semibold">$49</span>
-          </div>
-          <div className="text-sm text-gray-500 ">My description</div>
-          <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
-            Add to Cart
-          </button>
-        </Link>
-      ))}
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-ful h-80">
+          <Image
+            src="https://images.pexels.com/photos/26690666/pexels-photo-26690666/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/20852606/pexels-photo-20852606.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500 ">My description</div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
     </div>
   );
 };
