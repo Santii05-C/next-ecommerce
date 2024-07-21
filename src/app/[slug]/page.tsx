6527a79e38a855910810fb0319d491497497595d
Santii05-C <santi.c.pgm@gmail.com>
2024-07-21 17:32:45 -0300

    last commit

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,30 +2,32 @@ import Add from "@/components/Add";
 import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductImages from "@/components/ProductImages";
 import { wixClientServer } from "@/lib/wixClientServer";
+import { notFound } from "next/navigation";
 
 const SinglePage = async ({ params }: { params: { slug: string } }) => {
   const wixClient = await wixClientServer();
 
-  console.log(params.slug);
-  const res = await wixClient.products
+  const products = await wixClient.products
     .queryProducts()
-    // .eq("collectionIds", categoryId)
-
+    .eq("slug", params.slug)
     .find();
+
+  if (!products.items[0]) {
+    return notFound();
+  }
+
+  const product = products.items[0];
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16 ">
       {/* IMG */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-        <ProductImages />
+        <ProductImages items={product.media?.items} />
       </div>
       {/* TEXT */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6 ">
-        <h1 className="text-4xl font-medium">Product Name</h1>
-        <p className="text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
-          fugit maiores excepturi in corrupti officiis at eligendi nisi, quam,
-          odit eveniet qui ab sequi hic sit veritatis molestias eius unde.
-        </p>
+        <h1 className="text-4xl font-medium">{product.name}</h1>
+        <p className="text-gray-500">{product.description}</p>
         <div className="h-[2px] bg-gray-100" />
         <div className="flex items-center gap-4">
           <h3 className="text-xl text-gray-500 line-through">$59</h3>
--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -3,33 +3,33 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const images = [
-  {
-    id: 1,
-    url: "https://images.pexels.com/photos/18460661/pexels-photo-18460661/free-photo-of-ciudad-viaje-viajar-callejon.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-  },
-  {
-    id: 2,
-    url: "https://images.pexels.com/photos/19820544/pexels-photo-19820544/free-photo-of-nieve-nevar-madera-paisaje.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-  },
-  {
-    id: 3,
-    url: "https://images.pexels.com/photos/18939401/pexels-photo-18939401/free-photo-of-blanco-y-negro-ciudad-coches-trafico.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-  },
-  {
-    id: 4,
-    url: "https://images.pexels.com/photos/20877003/pexels-photo-20877003/free-photo-of-ciudad-edificios-pasos-urbano.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-  },
-];
+// const images = [
+//   {
+//     id: 1,
+//     url: "https://images.pexels.com/photos/18460661/pexels-photo-18460661/free-photo-of-ciudad-viaje-viajar-callejon.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+//   },
+//   {
+//     id: 2,
+//     url: "https://images.pexels.com/photos/19820544/pexels-photo-19820544/free-photo-of-nieve-nevar-madera-paisaje.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+//   },
+//   {
+//     id: 3,
+//     url: "https://images.pexels.com/photos/18939401/pexels-photo-18939401/free-photo-of-blanco-y-negro-ciudad-coches-trafico.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+//   },
+//   {
+//     id: 4,
+//     url: "https://images.pexels.com/photos/20877003/pexels-photo-20877003/free-photo-of-ciudad-edificios-pasos-urbano.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+//   },
+// ];
 
-const ProductImages = () => {
+const ProductImages = ({ items }: { items: any }) => {
   const [index, setIndex] = useState(0);
 
   return (
     <div className="">
       <div className="h-[500px] relative">
         <Image
-          src={images[index].url}
+          src={items[index].image?.url}
           alt=""
           fill
           sizes="50vw"
@@ -37,14 +37,14 @@ const ProductImages = () => {
         />
       </div>
       <div className="flex justify-between gap-4 mt-8">
-        {images.map((img, i) => (
+        {items.map((item: any, i: number) => (
           <div
             className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
-            key={img.id}
+            key={item._id}
             onClick={() => setIndex(i)}
           >
             <Image
-              src={img.url}
+              src={item.image?.url}
               alt=""
               fill
               sizes="30vw"
