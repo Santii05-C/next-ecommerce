b231e2ef6d8cbd743f0f7ec3cd3effa644f8b981
Santii05-C <santi.c.pgm@gmail.com>
2024-07-22 17:28:28 -0300

    last commit

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { products } from "@wix/stores";
+import { useState } from "react";
 
 const CustomizeProducts = ({
   productId,
@@ -11,22 +12,70 @@ const CustomizeProducts = ({
   variants: products.Variant[];
   productOptions: products.ProductOption[];
 }) => {
+  const [selectedOptions, setSelectedOptions] = useState<{
+    [key: string]: string;
+  }>({});
+
+  const handleOptionSelect = (optionType: string, choice: string) => {
+    setSelectedOptions((prev) => ({ ...prev, [optionType]: choice }));
+  };
+
+  const isVariantInStock = (choices: { [key: string]: string }) => {
+    return variants.some((variant) => {
+      const variantChoices = variant.choices;
+
+      if (!variantChoices) return false;
+
+      return (
+        Object.entries(choices).every(
+          ([key, value]) => variantChoices[key] === value
+        ) && variant.stock?.inStock
+      );
+    });
+  };
+
   return (
     <div className="flex flex-col gap-6">
       {productOptions.map((option) => (
-        <div className="flex fle-col gap-4" key={option.name}>
+        <div className="flex flex-col gap-4" key={option.name}>
           <h4 className="font-medium">Choose a {option.name}</h4>
+          {option.choices?.map((choice) => {
+            const disabled = !isVariantInStock({
+              ...selectedOptions,
+              [option.name!]: choice.description!,
+            });
+
+            const selected =
+              selectedOptions[option.name!] === choice.description;
+
+            return (
+              <div
+                className=""
+                key={choice.value}
+                onClick={() =>
+                  handleOptionSelect(option.name!, choice.description!)
+                }
+              >
+                {choice.description}
+                {disabled && "disable"}
+                {selected && "selected"}
+              </div>
+            );
+          })}
+        </div>
+      ))}
+      {/* COLOR */}
+      {/* 
           <ul className="flex items-center gap-3">
             <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500">
-              <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+              <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
             </li>
             <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-blue-500"></li>
             <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-not-allowed relative bg-green-500">
-              <div className="absolute w-10 h-[2px] bg-red-500 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+              <div className="absolute w-10 h-[2px] bg-red-500 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
             </li>
-          </ul>
-        </div>
-      ))}
+          </ul> */}
+      {/* OTHER */}
       {/* <h4 className="font-medium">Choose a size</h4>
       <ul className="flex items-center gap-3">
         <li className="ring-1 ring-lama text-lama rounded-md py-1 px-4 text-sm cursor-pointer">
