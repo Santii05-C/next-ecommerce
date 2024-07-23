3e9291462b6d9f9f085658fe490f404d6486d9f4
Santii05-C <santi.c.pgm@gmail.com>
2024-07-23 11:48:41 -0300

    last commit

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -29,39 +29,71 @@ const CustomizeProducts = ({
       return (
         Object.entries(choices).every(
           ([key, value]) => variantChoices[key] === value
-        ) && variant.stock?.inStock
+        ) &&
+        variant.stock?.inStock &&
+        variant.stock?.quantity &&
+        variant.stock?.quantity > 0
       );
     });
   };
+
+  console.log(variants);
 
   return (
     <div className="flex flex-col gap-6">
       {productOptions.map((option) => (
         <div className="flex flex-col gap-4" key={option.name}>
           <h4 className="font-medium">Choose a {option.name}</h4>
-          {option.choices?.map((choice) => {
-            const disabled = !isVariantInStock({
-              ...selectedOptions,
-              [option.name!]: choice.description!,
-            });
+          <ul className="flex items-center gap-3">
+            {option.choices?.map((choice) => {
+              const disabled = !isVariantInStock({
+                ...selectedOptions,
+                [option.name!]: choice.description!,
+              });
 
-            const selected =
-              selectedOptions[option.name!] === choice.description;
+              const selected =
+                selectedOptions[option.name!] === choice.description;
 
-            return (
-              <div
-                className=""
-                key={choice.value}
-                onClick={() =>
-                  handleOptionSelect(option.name!, choice.description!)
-                }
-              >
-                {choice.description}
-                {disabled && "disable"}
-                {selected && "selected"}
-              </div>
-            );
-          })}
+              const clickHandler = disabled
+                ? undefined
+                : () => handleOptionSelect(option.name!, choice.description!);
+
+              return option.name === "Color" ? (
+                <li
+                  className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative "
+                  style={{
+                    backgroundColor: choice.value,
+                    cursor: disabled ? "not-allowed" : "pointer",
+                  }}
+                  onClick={clickHandler}
+                >
+                  {selected && (
+                    <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                  {disabled && (
+                    <div className="absolute w-10 h-[2px] bg-red-500 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                </li>
+              ) : (
+                <li
+                  className="ring-1 ring-lama text-lama rounded-md py-1 px-4 text-sm "
+                  style={{
+                    cursor: disabled ? "not-allowed" : "pointer",
+                    backgroundColor: selected
+                      ? "#f35c7a"
+                      : disabled
+                      ? "#FBCFE8"
+                      : "white",
+                    color: selected || disabled ? " white" : "#f35c7a",
+                    boxShadow: disabled ? "none" : "",
+                  }}
+                  onClick={clickHandler}
+                >
+                  {choice.description}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       ))}
       {/* COLOR */}
