ebb116d732caff7cc9ff871bef40594c5d83f14c
Santii05-C <santi.c.pgm@gmail.com>
2024-07-25 12:34:47 -0300

    last commit

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+const Pagination = () => {
+  return (
+    <div className="rounded-md bg-lama text-white p-2 text-sm  w-24 cursor-pointer disabled:bg-pink-200">
+      Previous
+    </div>
+  );
+  return (
+    <div className="rounded-md bg-lama text-white p-2 text-sm  w-24 cursor-pointer disabled:bg-pink-200">
+      Next
+    </div>
+  );
+};
+
+export default Pagination;
