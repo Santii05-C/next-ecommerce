308677fb3294c9c0271f9f88246709ea0c447a46
Santii05-C <santi.c.pgm@gmail.com>
2024-07-25 12:55:33 -0300

    last commit

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -2,13 +2,13 @@
 
 const Pagination = () => {
   return (
-    <div className="rounded-md bg-lama text-white p-2 text-sm  w-24 cursor-pointer disabled:bg-pink-200">
-      Previous
-    </div>
-  );
-  return (
-    <div className="rounded-md bg-lama text-white p-2 text-sm  w-24 cursor-pointer disabled:bg-pink-200">
-      Next
+    <div className="mt-12 flex justify-between w-full">
+      <button className="rounded-md bg-lama text-white p-2 text-sm  w-24 cursor-pointer disabled:bg-pink-200">
+        Previous
+      </button>
+      <button className="rounded-md bg-lama text-white p-2 text-sm  w-24 cursor-pointer disabled:bg-pink-200">
+        Next
+      </button>
     </div>
   );
 };
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,6 +3,7 @@ import { products } from "@wix/stores";
 import Image from "next/image";
 import Link from "next/link";
 import DOMPurify from "isomorphic-dompurify";
+import Pagination from "./Pagination";
 
 const PRODUCT_PER_PAGE = 20;
 
@@ -89,6 +90,7 @@ const ProductList = async ({
           </button>
         </Link>
       ))}
+      <Pagination />
     </div>
   );
 };
