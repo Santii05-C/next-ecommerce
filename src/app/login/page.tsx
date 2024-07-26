81251a1d982f5915959d3ade4c19d899ccc78706
Santii05-C <santi.c.pgm@gmail.com>
2024-07-26 18:31:47 -0300

    last commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,53 @@
+"use client";
+
+import { useWixClient } from "@/hooks/useWixClient";
+import { useState } from "react";
+
+enum MODE {
+  LOGIN = "LOGIN",
+  REGISTER = "REGISTER",
+  RESET_PASSWORD = "RESET_PASSWORD",
+  EMAIL_VERIFICATION = "EMAIL_VERIFICATION",
+}
+
 const LoginPage = () => {
-  return <div className="">LoginPage</div>;
+  const [mode, setMode] = useState(MODE.LOGIN);
+
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailCode, setEmailCode] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+
+  const forTitle =
+    mode === MODE.LOGIN
+      ? "Log in"
+      : mode === MODE.REGISTER
+      ? "Register"
+      : mode === MODE.RESET_PASSWORD
+      ? "Reset Your Password"
+      : "Verify Your Email";
+
+  const buttonTitle =
+    mode === MODE.LOGIN
+      ? "Login "
+      : mode === MODE.REGISTER
+      ? "Register"
+      : mode === MODE.RESET_PASSWORD
+      ? "Reset  "
+      : "Verify";
+
+  const wixClient = useWixClient;
+
+  return (
+    <div className="h-[calc(100vh) - 80px] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center">
+      <form className="flex flex-col gap-8">
+        <div className="text-2sl">{formTitle}</div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
