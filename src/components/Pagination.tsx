dc3daffc822bc4bc5751860da3df46509bebbe4f
Santii05-C <santi.c.pgm@gmail.com>
2024-07-26 17:15:16 -0300

    last commit

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const Pagination = ({
   currentPage,
@@ -16,18 +15,24 @@ const Pagination = ({
   const searchParams = useSearchParams();
   const { replace } = useRouter();
 
-  const createPageUrl = (pageNumber: number) => {};
+  const createPageUrl = (pageNumber: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", pageNumber.toString());
+    replace(`${pathname}?${params.toString()}`);
+  };
   return (
     <div className="mt-12 flex justify-between w-full">
       <button
         className="rounded-md bg-lama text-white p-2 text-sm  w-24 cursor-pointer disabled:bg-pink-200 "
         disabled={!hasPrev}
+        onClick={() => createPageUrl(currentPage - 1)}
       >
         Previous
       </button>
       <button
         className="rounded-md bg-lama text-white p-2 text-sm  w-24 cursor-pointer disabled:bg-pink-200"
         disabled={!hasNext}
+        onClick={() => createPageUrl(currentPage + 1)}
       >
         Next
       </button>
