4bacafa3f16de77750c8d1cd6145258ff8332f7b
Santii05-C <santi.c.pgm@gmail.com>
2024-07-27 12:55:42 -0300

    last commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
 
-  const forTitle =
+  const formTitle =
     mode === MODE.LOGIN
       ? "Log in"
       : mode === MODE.REGISTER
@@ -42,9 +42,55 @@ const LoginPage = () => {
   const wixClient = useWixClient;
 
   return (
-    <div className="h-[calc(100vh) - 80px] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center">
+    <div className="h-[calc(100vh-80px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center">
       <form className="flex flex-col gap-8">
-        <div className="text-2sl">{formTitle}</div>
+        <h1 className="text-2sl font-semibold">{formTitle}</h1>
+        {mode === MODE.REGISTER ? (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="john"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
+        ) : null}
+        {mode !== MODE.EMAIL_VERIFICATION ? (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="john@gmail.com"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Verification Code</label>
+            <input
+              type="text"
+              name="emailCode"
+              placeholder="Code"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
+        )}
+        {mode === MODE.LOGIN || mode === MODE.REGISTER ? (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
+        ) : null}
+        <button className="bg-lama text-white p-2 rounded-md disabled:bg-pink-200 disabled:cursor-not-allowed">
+          {buttonTitle}
+        </button>
       </form>
     </div>
   );
