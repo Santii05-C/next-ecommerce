f98de533d721e5ed632c7592cdc40a920a62a176
Santii05-C <santi.c.pgm@gmail.com>
2024-07-30 16:53:17 -0300

    last commit

--- src/components/CarModal.tsx
+++ src/components/CarModal.tsx
@@ -6,8 +6,8 @@ import { media as wixMedia } from "@wix/sdk";
 import { useWixClient } from "@/hooks/useWixClient";
 import { currentCart } from "@wix/ecom";
 
-const CarModal = () => {
-  //TEMPORARY
+const CartModal = () => {
+  // TEMPORARY
   // const cartItems = true;
 
   const wixClient = useWixClient();
@@ -28,6 +28,7 @@ const CarModal = () => {
             thankYouPageUrl: `${window.location.origin}/success`,
           },
         });
+
       if (redirectSession?.fullUrl) {
         window.location.href = redirectSession.fullUrl;
       }
@@ -35,11 +36,11 @@ const CarModal = () => {
       console.log(err);
     }
   };
-  //4:57
+
   return (
     <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
       {!cart.lineItems ? (
-        <div className="">Card is Empty</div>
+        <div className="">Cart is Empty</div>
       ) : (
         <>
           <h2 className="text-xl">Shopping Cart</h2>
@@ -47,7 +48,7 @@ const CarModal = () => {
           <div className="flex flex-col gap-8">
             {/* ITEM */}
             {cart.lineItems.map((item) => (
-              <div className="flex gap-4 " key={item._id}>
+              <div className="flex gap-4" key={item._id}>
                 {item.image && (
                   <Image
                     src={wixMedia.getScaledToFillImageUrl(
@@ -66,21 +67,21 @@ const CarModal = () => {
                   {/* TOP */}
                   <div className="">
                     {/* TITLE */}
-                    <div className=" flex items-center justify-between gap-8">
+                    <div className="flex items-center justify-between gap-8">
                       <h3 className="font-semibold">
                         {item.productName?.original}
                       </h3>
                       <div className="p-1 bg-gray-50 rounded-sm flex items-center gap-2">
                         {item.quantity && item.quantity > 1 && (
                           <div className="text-xs text-green-500">
-                            {item.quantity} x
+                            {item.quantity} x{" "}
                           </div>
                         )}
                         ${item.price?.amount}
                       </div>
                     </div>
                     {/* DESC */}
-                    <div className="tex-sm text-gray-500">
+                    <div className="text-sm text-gray-500">
                       {item.availability?.status}
                     </div>
                   </div>
@@ -106,15 +107,16 @@ const CarModal = () => {
               <span className="">${cart.subtotal.amount}</span>
             </div>
             <p className="text-gray-500 text-sm mt-2 mb-4">
-              Lorem ipsum dolor sit amet, consectetur.
+              Shipping and taxes calculated at checkout.
             </p>
             <div className="flex justify-between text-sm">
-              <button className=" rounded-md py-3 px-4 ring-1 ring-gray-300">
+              <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">
                 View Cart
               </button>
               <button
                 className="rounded-md py-3 px-4 bg-black text-white disabled:cursor-not-allowed disabled:opacity-75"
                 disabled={isLoading}
+                onClick={handleCheckout}
               >
                 Checkout
               </button>
@@ -126,4 +128,4 @@ const CarModal = () => {
   );
 };
 
-export default CarModal;
+export default CartModal;
